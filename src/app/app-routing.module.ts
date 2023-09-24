968efd58e814caa7bcaed28adf6ce71e700d4df1
Srilekha <srichandraleka@gmail.com>
2023-09-24 10:08:55 +0530

    MatrimonyApp

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { IonicSearch } from './ionic-search/image-view.component';
+import { IonicViewComponent } from './ionic-view/ionic-view.component';
+import { SwipeScreenComponent } from './swipe-screen/swipe-screen.component';
+
+const routes: Routes = [
+  { path: '', component: IonicSearch },
+  { path: 'view/:id/:name', component: IonicViewComponent},
+  { path: 'swipe/:id/:name', component: SwipeScreenComponent},
+  { path: '**', redirectTo: '' }
+ 
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
